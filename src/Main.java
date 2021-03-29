1ac8c027108c6a7a7b008081e1463df6c730dca2
ggujangi <asmwj3@naver.com>
2021-03-30 03:33:59 +0900

    Add Main class

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import solution.Ex_485;
+
+public class Main {
+    public static void main(String[] args) {
+        Ex_485 solution = new Ex_485();
+        System.out.print(solution.findMaxConsecutiveOnes(new int[]{1, 1, 0, 1, 1}));
+    }
+}
