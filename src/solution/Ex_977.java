8f927d05eed4f4dd91d6430d95d5646e7e5c6e6a
ggujangi <asmwj3@naver.com>
2021-03-30 23:50:08 +0900

    [submit 2] #977

--- src/solution/Ex_977.java
+++ src/solution/Ex_977.java
@@ -6,16 +6,15 @@ import java.util.Arrays;
  * #977
  * Squares of a Sorted Array
  *
- * 3 ms	40.9 MB
+ * 2 ms	40.8 MB
  */
 
 public class Ex_977 {
     public int[] sortedSquares(int[] nums) {
-        int[] s = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
-            s[i] = nums[i] * nums[i];
+            nums[i] = nums[i] * nums[i];
         }
-        Arrays.sort(s);
-        return s;
+        Arrays.sort(nums);
+        return nums;
     }
 }
