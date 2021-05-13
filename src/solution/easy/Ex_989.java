8357ed9efafddab21dcafabc5e9e46c154be10f5
ggujangi <asmwj3@naver.com>
2021-05-13 16:06:10 +0900

    [submit 3] #989

--- src/solution/easy/Ex_989.java
+++ src/solution/easy/Ex_989.java
@@ -1,31 +1,28 @@
 package solution.easy;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
  * #989
  * Add to Array-Form of Integer
  *
- * 3 ms 40 MB
+ * 3 ms 41.3 MB
  */
 
 public class Ex_989 {
     public List<Integer> addToArrayForm(int[] num, int k) {
-        List<Integer> result = new ArrayList<>();
+        LinkedList<Integer> list = new LinkedList<>();
 
         int i = num.length - 1, sum = k;
 
         while (i >= 0 || sum > 0) {
             if (i >= 0) sum += (num[i]);
-            result.add(sum % 10);
+            list.addFirst(sum % 10);
             sum /= 10;
             i--;
         }
 
-        Collections.reverse(result);
-
-        return result;
+        return list;
     }
 }
