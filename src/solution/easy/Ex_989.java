eda4f82f5b86cc8da892d136c38397334098f976
ggujangi <asmwj3@naver.com>
2021-05-13 16:03:26 +0900

    [submit 2] #989

--- src/solution/easy/Ex_989.java
+++ src/solution/easy/Ex_989.java
@@ -8,35 +8,20 @@ import java.util.List;
  * #989
  * Add to Array-Form of Integer
  *
- * 3 ms 40.2 MB
+ * 3 ms 40 MB
  */
 
 public class Ex_989 {
     public List<Integer> addToArrayForm(int[] num, int k) {
         List<Integer> result = new ArrayList<>();
 
-        int i = num.length - 1;
-        int sum = 0;
+        int i = num.length - 1, sum = k;
 
-        while (i >= 0 || k > 0) {
-            if (i >= 0) {
-                sum += num[i];
-                i--;
-            }
-
-            if (k > 0) {
-                sum += k % 10;
-                k /= 10;
-            }
-
+        while (i >= 0 || sum > 0) {
+            if (i >= 0) sum += (num[i]);
             result.add(sum % 10);
-
-            if (sum > 9) {
-                if (i < 0 && k == 0) result.add(1);
-                sum /= 10;
-            } else {
-                sum = 0;
-            }
+            sum /= 10;
+            i--;
         }
 
         Collections.reverse(result);
