bcaa4efaf46d3a840a8531b8dbb6e50ff34cdcf0
ggujangi <asmwj3@naver.com>
2021-05-14 15:21:30 +0900

    [submit 2] #561

--- src/solution/easy/Ex_561.java
+++ src/solution/easy/Ex_561.java
@@ -1,21 +1,34 @@
 package solution.easy;
 
-import java.util.Arrays;
-
 /**
  * #561
  * Array Partition I
  *
- * 10 ms 40.7 MB
+ * 3 ms 40.9 MB
  */
 
 public class Ex_561 {
     public int arrayPairSum(int[] nums) {
-        int result = 0;
-        Arrays.sort(nums);
-        for(int i = 0; i < nums.length; i=i+2){
-            result+=nums[i];
+        int[] arr = new int[20001];
+        int lim = 10000, sum = 0;
+        for (int n : nums) arr[n + lim]++;
+
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i = i + 2) {
+            while (arr[k] == 0) k++;
+
+            sum += k - lim;
+
+            if (arr[k] == 1) {
+                k++;
+                while (arr[k] == 0) k++;
+                arr[k]--;
+            } else {
+                arr[k] -= 2;
+            }
         }
-        return result;
+
+        return sum;
     }
 }
